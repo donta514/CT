111080762845b6249c94bdf27e0ae600da718a13
donta514 <58492506+donta514@users.noreply.github.com>
2022-02-28 08:33:53 -0500

    start using index.html file

--- client/storyscript.js
+++ client/storyscript.js
@@ -5,7 +5,70 @@
 // }
 
 // updateStory();
+// imageLink = imageLink.includes("/s3/files")
+//   ? `https://newsbusters.org${imageLink}`
+//   : imageLink;
+fetch("https://newsbusters.org/api/fsarecent")
+  .then((data) => {
+    //   console.log(data);
+    return data.json();
+  })
+  .then((completedata) => {
+    // console.log(completedata);
 
-imageLink = imageLink.includes("/s3/files")
-  ? `https://newsbusters.org${imageLink}`
-  : imageLink;
+    let stories = "";
+    completedata.map((values) => {
+      stories += `<tr class="article" pardot-repeatable="">
+        <td class="two-column">
+            <div class="column">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td class="inner">
+                                <table class="contents">
+                                    <tbody>
+                                        <tr>
+                                            <td><a class="article-link" href=${values.link}
+                                                    target="_blank"><img alt=""
+                                                        src=${values.image}> </a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="column">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td class="inner">
+                                <table class="contents">
+                                    <tbody>
+                                        <tr>
+                                            <td class="text">
+                                                <h3 class="title">
+                                                    <a href=${values.link}
+                                                        target="_blank">${values.title}</a>&nbsp;
+                                                </h3>
+                                                <p class="authorDate">
+                                                    By <span>${values.author}</span> | ${values.created}</p>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </td>
+    </tr>`;
+    });
+    document.getElementById("articles").innerHTML = stories;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
